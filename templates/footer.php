16d59f1f65e9134c49ebb03dd2bb62785bae75dd
Mike Prior <mprior19xx@gmail.com>
2020-02-10 11:00:39 -0500

    hey

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
 <router-link to="/">Go to Home</router-link>
 <router-link to="/app">Go to Sign Up</router-link>    
-This is the footer <br> Copyright <?php echo date('Y'); ?>
+This is the footer <br> Copyright sadf<?php echo date('Y'); ?>
 </footer>
