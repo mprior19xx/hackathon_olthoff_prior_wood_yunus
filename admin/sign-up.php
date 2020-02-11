1fd16cee24392391c8f3c2b5362292b1cf6a3259
Philip <philipwood0912@gmail.com>
2020-02-10 21:06:33 -0500

    added mail functionality

--- admin/sign-up.php
+++ admin/sign-up.php
@@ -15,9 +15,32 @@
             exit;
         }
     }
+    //function for sending mail
+    function send_email($full_name, $email, $subject, $mail_message){
+        //filter name
+        $full_name = filter_var($full_name, FILTER_SANITIZE_STRING);
+        //filter email
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $email);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        //filter subject
+        $subject = filter_var($subject, FILTER_SANITIZE_STRING);
+        //add headers
+        $headers = array(
+            'From'=>'a_wood60810@fanshaweonline.ca',
+            'Reply-To'=>$full_name.'<'.$email.'>'
+        );
+        //echo mail($email, $subject, $mail_message, $headers);
+        return mail($email, $subject, $mail_message, $headers);
+    }
 
     function db_add($date, $fname, $lname, $email, $country) {
+        //db connection
         $pdo = Database::getInstance()->getConnection();
+        //mail variables
+        $full_name = $fname.' '.$lname;
+        $subject = '';
+        $mail_message = '';
+        //initial db query
         $check_query = 'SELECT COUNT(*) FROM tbl_signup WHERE Email =:email';
         $db_check = $pdo->prepare($check_query);
         $db_check->execute(
@@ -25,6 +48,7 @@
                 ':email'=>$email
             )
         );
+        //look for match in current query
         if($db_check->fetchColumn()>0){
             $match_query = 'SELECT * FROM `tbl_signup` WHERE Email =:email';
             $match_email = $pdo->prepare($match_query);
@@ -33,6 +57,7 @@
                         ':email'=>$email
                     )
                 );
+                //if match is found, update date
             while($found_match = $match_email->fetch(PDO::FETCH_ASSOC)){
                 $id = $found_match['ID'];
                 $id_query = 'UPDATE tbl_signup SET Update_Date =:dat WHERE ID =:id';
@@ -43,8 +68,12 @@
                         ':id'=>$id
                     )
                 );
+                $subject = "Re-subscription";
+                $mail_message = "Thank you for re-subscribing to our newsletter!";
+                send_email($full_name, $email, $subject, $mail_message);
                 return "Thanks for re-subscribing to our newsletter!";
             }
+            //if no match is found, insert into db
         }else{
             $submit_query = 'INSERT INTO `tbl_signup` (F_Name, L_Name, Email, Country, Subscribe_Date, Update_Date ) VALUES (:fname, :lname, :email, :country, :dat, :dat);';
             $signup_set = $pdo->prepare($submit_query);
@@ -57,6 +86,9 @@
                     ':dat'=>$date
                 )
             );
+            $subject = "Subscription";
+            $mail_message = "Thank you for subscribing to our newsletter!";
+            send_email($full_name, $email, $subject, $mail_message);
             return "Thanks for subscribing to our newsletter!";
         }
     }
